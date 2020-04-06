a0a4c69e78e5c43b2db73dd64cc19db523ce54b3
John White <jaydub@MacBook-Pro-3.attlocal.net>
2020-04-06 10:46:22 -0400

    push to pages

---
+++ build/precache-manifest.3c322a4366626925325a0c0c61e8d15f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d25b926a446c41a244b90ae73a0dd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3693c18714674b28089e",
+    "url": "/static/css/main.86a2b594.chunk.css"
+  },
+  {
+    "revision": "2f9feee091b8e9950428",
+    "url": "/static/js/2.c6066ef0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c6066ef0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3693c18714674b28089e",
+    "url": "/static/js/main.60446c3f.chunk.js"
+  },
+  {
+    "revision": "36939c150940507d8bc9",
+    "url": "/static/js/runtime-main.01edf537.js"
+  }
+]);
