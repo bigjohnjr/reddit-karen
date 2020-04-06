8eab6c213f3eedd0afcd8c2cc5ad4e781d66f325
John White <jaydub@MacBook-Pro-3.attlocal.net>
2020-04-06 10:13:18 -0400

    finish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-import {Card, Button} from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
+import {CardColumns, Card, Button} from 'react-bootstrap';
 import './App.css';
 
 class App extends React.Component {
@@ -33,21 +34,25 @@ class App extends React.Component {
         let defaultImage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTeoUqBlHWsYFpxU77nOWI7xFr7rLA4O5h5UrpTYut-2s-0MzJi&usqp=CAU';
         return (
             <>
-                <div className="ui container" style={{ marginTop: '10px'}}>
-                    HI REDDIT
+                <Container style={{ marginTop: '10px'}}>
+                    <h1 className="my-3">Karen<span className="orange-bg">hub</span></h1>
+                    <CardColumns>
                     {this.state.Posts.map((item,i) => (
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src={item.preview ? item.preview.images[0].source.url : defaultImage} />
-                            <Card.Body>
-                                <Card.Title>{item.title}</Card.Title>
-                                    <Card.Text>
-                                        {this.truncateText(item.selftext, 100)}
-                                    </Card.Text>
-                                <Button as="a" href={item.url} target="_blank" variant="primary">READ MORE</Button>
-                            </Card.Body>
-                        </Card>
+
+                            <Card>
+                                <Card.Img variant="top" src={item.preview ? item.preview.images[0].source.url : defaultImage} />
+                                <Card.Body>
+                                    <Card.Title>{item.title}</Card.Title>
+                                        <Card.Text>
+                                            {this.truncateText(item.selftext, 100)}
+                                        </Card.Text>
+                                    <Button as="a" href={item.url} target="_blank" variant="primary">READ MORE</Button>
+                                </Card.Body>
+                            </Card>
+
                     ))}
-                </div>
+                    </CardColumns>
+                </Container>
             </>
         );
     }
